e0eb804e949a6924850d03b6c747df5bf43ea343
Eze Amadi <eze.kachi.amadi@gmail.com>
2020-05-07 20:54:39 -0500

    timer animation element start

--- js/index.js
+++ js/index.js
@@ -2,6 +2,19 @@ const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 const durationInput = document.querySelector('#duration');
 
+// for the circle
+
+const circle = document.querySelector('circle');
+// getting the radius property of the svg circle
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+// setting the perimtere of the svg
+circle.setAttribute('stroke-dasharray', perimeter);
+
+// variables to help in the timer class for calculations
+
+let currentOffset = 0;
+// creating am instance of our class timer.
+
 const timer = new Timer(durationInput, startButton, pauseButton, {
 	// Optional Callbacks that will be used...
 	onStart() {
@@ -13,6 +26,7 @@ const timer = new Timer(durationInput, startButton, pauseButton, {
 	},
 
 	onTick() {
-		console.log('Timer is ticking');
+		circle.setAttribute('stroke-dashoffset', currentOffset);
+		currentOffset -= 5;
 	}
 });
